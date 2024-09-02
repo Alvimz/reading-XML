48a7559510a8150695cc1d7564db501e69908ba3
gAlvim <ledyzito@hotmail.com>
2024-09-02 15:22:18 -0300

    Add:RemoveTag

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 from bs4 import BeautifulSoup
 
 class Logic:
-    def get_site_xml():
+    def __init__(self) -> None:
+        self.search_tag= [] #lista para armazenar as tags
+    
+    #pega linha por linha do xml e armazena na lista 'search_tag'
+    
+    def get_site_xml(self):
         with open("urls.xml", "r") as file:
             line = file.read()
         content = BeautifulSoup(line,'xml') 
-        search_tag = content.find_all('site') #procurando por tag
-        return search_tag #retornando em lista os valores
-        
+        self.search_tag = content.find_all('site') #procurando por tag
+        return self.search_tag #retornando em lista com os valores
     
-a = Logic
-lista_xml = a.get_site_xml()
-print(lista_xml[1])
+    def remove_tag(self):
+        for item in self.search_tag:
+            print(item.text)
+        ...
+        
+
+a = Logic()
+
+a.get_site_xml()
+a.remove_tag()
+
