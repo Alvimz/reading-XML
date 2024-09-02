58d8cfbf8c14dcea1d0713c005aa7d08e6c62ecb
gAlvim <ledyzito@hotmail.com>
2024-09-02 14:40:59 -0300

    Add:logica get lines

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+from bs4 import BeautifulSoup
+
 class Logic:
-    def readingxml():
-        with open('urls.xml','r') as file:
-            for lines in file:
-                print(lines.strip())
-                
+    def get_site_xml():
+        with open("urls.xml", "r") as file:
+            line = file.read()
+        content = BeautifulSoup(line,'xml') 
+        search_tag = content.find_all('site') #procurando por tag
+        return search_tag #retornando em lista os valores
+        
+    
 a = Logic
-a.readingxml()
+lista_xml = a.get_site_xml()
+print(lista_xml[1])
