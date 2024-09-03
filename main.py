3dc28168c08128162c59ac603acc76d811a92fa2
gAlvim <ledyzito@hotmail.com>
2024-09-03 11:34:05 -0300

    renomeia variaveis e funcoes

--- main.py
+++ main.py
@@ -15,50 +15,50 @@ from openpyxl import load_workbook, Workbook
 
 class Logic:
     def __init__(self) -> None:
-        self.search_tag = []  # lista para armazenar as tags diretamente do xml
-        self.result_links = []  # lista com os links válidos!
-        self.error_links = []  # lista com os links errôneos
+        self.links_raw = []  # lista para armazenar as tags diretamente do xml
+        self.links_validated = []  # lista com os links válidos!
+        self.links_error = []  # lista com os links errôneos
 
-    def get_site_xml(self):  # pega linha por linha do xml e armazena na lista 'search_tag'
+    def get_url_xml(self):  # pega linha por linha do xml e armazena na lista 'links_raw'
         with open("urls.xml", "r") as file:
             line = file.read()
         content = BeautifulSoup(line, "xml")
 
         for item in content.find_all("site"):  # salva apenas o link na lista
-            self.search_tag.append(item.text)
+            self.links_raw.append(item.text)
 
-        return self.search_tag  # retornando em lista com os valores
+        return self.links_raw  # retornando em lista com os valores
 
     def check_url(self):  # testa as urls e as separa se funfa ou não!
-        for urls in self.search_tag:
+        for urls in self.links_raw:
             try:
                 maybe_url = requests.get(urls)
                 if maybe_url.status_code == 200:
-                    self.result_links.append(urls)
+                    self.links_validated.append(urls)
 
             except:
-                self.error_links.append(urls)
+                self.links_error.append(urls)
 
-        return self.result_links, self.error_links
+        return self.links_validated, self.links_error
 
     def add_link_xlsx(self):  # passa os links para a planilha!
         try:
             # carrega a planilha caso ela não exista
-            wb = load_workbook("links.xlsx")
-            workbook_active = wb.active
+            work_book = load_workbook("links.xlsx")
+            workbook_active = work_book.active
         except:
             # cria a planilha!
-            wb = Workbook()
-            workbook_active = wb.active
-        for link in self.result_links:  # adiciona os links válidos
+            work_book = Workbook()
+            workbook_active = work_book.active
+        for link in self.links_validated:  # adiciona os links válidos
             workbook_active.append([link, "Link válido!"])
-        for link in self.error_links:  # adiciona os links não válidos!
+        for link in self.links_error:  # adiciona os links não válidos!
             workbook_active.append([link, "Link inválido!"])
-        wb.save("links.xlsx")
+        work_book.save("links.xlsx")
 
 
 a = Logic()
-a.get_site_xml()
+a.get_url_xml()
 # print(a.requesition_links())
 result_links, error_links = a.check_url()
 a.add_link_xlsx()
