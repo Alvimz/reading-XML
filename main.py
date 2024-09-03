73bc8e60ed620dee776618b377eaec4d3455f685
gAlvim <ledyzito@hotmail.com>
2024-09-03 12:26:16 -0300

    Add:Função de espaços vazios na linha xml

--- main.py
+++ main.py
@@ -5,24 +5,32 @@ from openpyxl import load_workbook, Workbook
 #recebedor de arquivo!
     #testa se é um arquivo xml! ✔️
 #verificar se tem a estrutura de um link!
-#verificar espaços em brancos no xml!
+#verificar espaços em brancos no xml! ✔️
 
 class Logic:
     def __init__(self) -> None:
         self.links_raw = []  # lista para armazenar as tags diretamente do xml
         self.links_validated = []  # lista com os links válidos!
         self.links_error = []  # lista com os links errôneos
-        self.supose_file = None
+        self.supose_path_xml = None
         
 
     def file_tester_xml(self):
-        supose_path_xml = input('Digite o diretório do arquivo XML!')
-        if '.xml'in supose_path_xml:
-            return True,supose_path_xml
-        return False
+        self.supose_path_xml = input('Digite o diretório do arquivo XML!')
+        if '.xml'in self.supose_path_xml:
+            return True,self.supose_path_xml
+        return False,None
     
-    def check_empty_box_xml(self):
-        ...
+    def check_empty_line_xml(self):
+        if self.supose_path_xml != None:
+            with open(self.supose_path_xml,'r') as file:
+                for linha in file:
+                    if linha.strip() == '':
+                        continue
+                return True
+        else:
+            return False
+        
         
     def get_url_xml(self):  # pega linha por linha do xml e armazena na lista 'links_raw'
         with open("urls.xml", "r") as file:
@@ -65,6 +73,7 @@ class Logic:
 
 a = Logic()
 print(a.file_tester_xml())
+print(a.check_empty_line_xml())
 # a.get_url_xml()
 # print(a.requesition_links())
 # result_links, error_links = a.check_url_xml()
