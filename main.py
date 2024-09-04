290088d9e4fac2bf0ca932d6f5cf8487d5306c5c
gAlvim <ledyzito@hotmail.com>
2024-09-04 14:20:02 -0300

    Add:funcao para pegar paths

--- main.py
+++ main.py
@@ -2,17 +2,19 @@ from bs4 import BeautifulSoup
 import requests
 from openpyxl import load_workbook, Workbook
 import re
+import os
 
 #recebedor de arquivo!
     #Verifica peso do arquivo! ❌
+    #verifica se o que o cara digitou é vazio❌ 
     #testa se é um arquivo xml! ✔️
 #verificador de link
     #verificar se tem a estrutura de um link e o adiciona na lista! ❌
     #verificar espaços em brancos no xml e o add na lista! ✔️
     #reformular os códigos e tirar esse mundo de lista! ❌
 #distribui os links em htmls
-    #criação da pasta para inserção dos htmls!
-    #criação arquivo htmls com o resultado do get!
+    #criação da pasta para inserção dos htmls!❌
+    #criação arquivo htmls com o resultado do get!❌
 """
 Pessoa introduz um xml, ele pegará todos os links(tirando os espaços vazios , descartando os digitados errados, linhas em brancos, coisas que
 forem links!), dará um get e salvará o retorno em um arquivo a parte!
@@ -28,10 +30,26 @@ class Logic:
         self.links_error = []  # lista com os links errôneos
         self.supose_path_xml = None #path do xml!
         
-    
+    def create_path_html(self,path):
+        pasta_name = 'Resultado_xml!'
+        path_complete = os.path.join(path,pasta_name)
+        if not os.path.exists(path_complete):
+            os.makedirs(path_complete)
+            print('Pasta criada com sucesso!')
+            
+    def get_paths_xml_lib(self):
+        self.supose_path_xml = input('Digite o diretório do arquivo XML!')
+        if self.supose_path_xml.endswith('.xml'):
+            self.supose_path_lib = input('Digite agora onde deseja salvar a pasta de retorno do xml! ')
+            self.create_path_html(self.supose_path_lib)
+            return self.supose_path_xml
+            
+            
     def path_tester_xml(self):#testa se é um xml o que a pessoa está introduzindo!
         self.supose_path_xml = input('Digite o diretório do arquivo XML!')
-        if '.xml'in self.supose_path_xml:
+        
+        # self.create_path_html(self.supose_path_xml)
+        if '.xml'in self.supose_path_xml: #.endswith('.xml')
             return True,self.supose_path_xml
         return False,None
     
@@ -39,7 +57,7 @@ class Logic:
         answer = input('>')
         if not answer:
             return True
-        
+
             
         
         
@@ -80,7 +98,10 @@ class Logic:
 
         return self.links_validated, self.links_error
 
-    def create_path_html():
+
+        
+        
+        
         ...
         
     def create_htmls():
@@ -104,7 +125,8 @@ class Logic:
   
 
 a = Logic()
-a.path_tester_xml()
+
+a.get_paths()
 print(a.get_line_from_xml())
 a.check_if_its_url()
 print(a.links_validated,'--',a.links_error)
