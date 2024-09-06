d3c1b820db418f97ae6e68f205c62a6a3a82c2ce
gAlvim <ledyzito@hotmail.com>
2024-09-06 17:17:29 -0300

    Cria gerador de path html e gerador de link para output

---
+++ generators_paths.py
@@ -0,0 +1,18 @@
+from verification import Verifications
+import os
+class Paths:
+    
+        
+    @staticmethod    
+    def create_path_html(path):  # cria a pasta para receber os html
+        if not Verifications.this_file_exists(path):
+            os.makedirs(path)
+            print("Pasta criada com sucesso!")
+        else:
+            print("Pasta para os registros já existe!")
+            
+    def generate_html_path(self,path_lib):
+        pasta_nome = "Resultado_xml"
+        self.path_html_output=os.path.join(path_lib,pasta_nome)
+          # alterar para join futuramente!🟨
+        return self.path_html_output
