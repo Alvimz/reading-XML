f0b19e3a9fc544e5e944fcb460e568164b9b81ba
gAlvim <ledyzito@hotmail.com>
2024-10-15 14:19:26 -0300

    criação pool request

--- poolrequest.py
+++ poolrequest.py
@@ -1,5 +1,10 @@
-import threading
+from report import Report
 from concurrent.futures import ThreadPoolExecutor
 class PoolRequest:
     ...
     
+    def process_request(self,request,path_save):
+        report = Report()
+        with ThreadPoolExecutor(max_workers=3) as executor:
+            for obj in report.get_list(path_save):
+                executor.submit(report.save,obj)
