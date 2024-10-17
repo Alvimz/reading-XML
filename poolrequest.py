301ef496e190bcccd1589709c8977882994079ec
gAlvim <ledyzito@hotmail.com>
2024-10-17 15:07:01 -0300

    pool request criado

--- poolrequest.py
+++ poolrequest.py
@@ -1,21 +1,17 @@
-from concurrent.futures import ThreadPoolExecutor
-from http_helper import HTTPHelper
-from report import Report
-class PoolRequest:
-    def __init__(self) -> None:
-        self.executor = ThreadPoolExecutor(max_workers=3)
-        self.threads_ = list()
-        self.report = Report()
-    
-    def pool_request(self,request_list):
-        for url in request_list:
-            _thread = self.executor.submit(self.process_url,url)
-            self.threads_.append(_thread)
-            
-    def process_url(self,url):
-        content_url = HTTPHelper.get_url_content(url)
-        self.report.add_item(content_url)
-    
-    def wait_complet(self):
-        self.executor.shutdown(wait=True)
-        
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+class MaybeThreads:
+    def __init__(self):
+        self.exec = ThreadPoolExecutor(max_workers=5)
+        self._threads = list()
+
+    def run(self, func, *args):
+        future = self.exec.submit(func, *args)
+        self._threads.append(future)
+
+    def wait_4_complete(self):
+        for future in as_completed(self._threads):
+            future.result()
+
+    def shutdown(self):
+        self.exec.shutdown(wait=True)
