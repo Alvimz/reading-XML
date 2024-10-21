5b216dd17e251bc9249822a378289642d5a92b66
gAlvim <ledyzito@hotmail.com>
2024-10-21 14:13:03 -0300

    mexendo no log temporary

--- temporary_log.py
+++ temporary_log.py
@@ -2,13 +2,21 @@ import os
 
 class TemporaryLog:
     def __init__(self) -> None:
-        ...
+        self.create_file_temporary
         
     def create_file_temporary(self):
         if not os.path.exists('log_temporary.txt'):
             with open('log_temporary.txt','w') as f:
                 f.close()
+    
+    def write_log(self,time:float):
+        with open('log_temporary.txt','a') as f:
+            f.write(f'{time}\n')
             
+    def read_log(self):
+        with open('log_temporary.txt','r') as f:
+            for linha in f:
+                ...
 
 a = TemporaryLog()
 a.create_file_temporary()
