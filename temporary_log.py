3d7c3c0a06f3fac58383fb33bfbf999d067b30da
gAlvim <ledyzito@hotmail.com>
2024-10-21 14:31:54 -0300

    terminado temporary_log

--- temporary_log.py
+++ temporary_log.py
@@ -1,8 +1,10 @@
 import os
-
+from xlsx_manager import XlsxManager
 class TemporaryLog:
     def __init__(self) -> None:
         self.create_file_temporary
+        self.logs = list()
+        self.avg_qnt = 10
         
     def create_file_temporary(self):
         if not os.path.exists('log_temporary.txt'):
@@ -13,11 +15,26 @@ class TemporaryLog:
         with open('log_temporary.txt','a') as f:
             f.write(f'{time}\n')
             
-    def read_log(self):
+    def save_csv_log(self):
+        xls_manager = XlsxManager()
         with open('log_temporary.txt','r') as f:
             for linha in f:
-                ...
+                self.logs.append(linha)
+            if len(self.logs) == self.avg_qnt:
+                avg = self.average_numbers(self.logs)
+                xls_manager.save_to_log(avg)
+                self.clear_temporary_log()
+                
+                
+    def average_numbers(self,list_numbers):
+        avg = sum(list_numbers)/self.avg_qnt
+        return avg
 
+    def clear_temporary_log(self):
+        if os.path.exists('log_temporary.txt'):
+            with open('log_temporary.txt', 'w') as f:
+                f.write('')
+                
 a = TemporaryLog()
 a.create_file_temporary()
 
