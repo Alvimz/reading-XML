ddb4ad1e10047bae36ccedb0c3343f683bc5e2c6
gAlvim <ledyzito@hotmail.com>
2024-10-21 15:37:43 -0300

    log_terminado

--- temporary_log.py
+++ temporary_log.py
@@ -3,8 +3,8 @@ from xlsx_manager import XlsxManager
 class TemporaryLog:
     def __init__(self) -> None:
         self.create_file_temporary
-        self.logs = list()
         self.avg_qnt = 10
+        self.raw_numbers = list()
         
     def create_file_temporary(self):
         if not os.path.exists('log_temporary.txt'):
@@ -15,18 +15,8 @@ class TemporaryLog:
         with open('log_temporary.txt','a') as f:
             f.write(f'{time}\n')
             
-    def save_csv_log(self):
-        xls_manager = XlsxManager()
-        with open('log_temporary.txt','r') as f:
-            for linha in f:
-                self.logs.append(linha.strip())
-            avg = self.average_numbers(self.logs)
-            xls_manager.save_to_log(avg)
-            self.clear_temporary_log()
-                
-                
-    def average_numbers(self,list_numbers):
-        avg = sum(list_numbers)/self.avg_qnt
+    def average_numbers(self):
+        avg = sum(self.raw_numbers)/self.avg_qnt
         return avg
 
     @staticmethod
@@ -34,6 +24,24 @@ class TemporaryLog:
         if os.path.exists('log_temporary.txt'):
             with open('log_temporary.txt', 'w') as f:
                 f.write('')
+            
+    def save_csv_log(self):
+        xls_manager = XlsxManager()
+        avg = self.average_numbers() #chama outra função para caclular
+        xls_manager.save_to_log(avg)
+        self.clear_temporary_log()
+            
+    def get_numbers(self):
+        with open('log_temporary.txt','r') as f:
+            for line in f:
+                line_number = float(line.strip())
+                self.raw_numbers.append(line_number)
+            return self.raw_numbers
+        
+        
+                
+                
+    
                 
 a = TemporaryLog()
 
