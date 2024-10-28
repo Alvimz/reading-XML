0c3b8f5d2d3888fe27f193e503952cf364487b60
gAlvim <ledyzito@hotmail.com>
2024-10-28 16:00:34 -0300

    cria o decorador do time_passed

--- time_passed.py
+++ time_passed.py
@@ -1,10 +1,15 @@
 import time
 
 class TimePassed:
-    def define_time_set(self,action)->float:
-        start_time= time.time()
-        action()
-        elapsed_time = time.time() - start_time
-        return elapsed_time
-    
+    def __init__(self, func) -> None:
+        self.func = func
+        self.execution_time = None  
 
+    def __call__(self, *args, **kwargs) -> float:
+        start_time = time.time()
+        result = self.func(*args, **kwargs)  
+        self.execution_time = time.time() - start_time
+        return result 
+
+    def get_execution_time(self) -> float:
+        return self.execution_time
