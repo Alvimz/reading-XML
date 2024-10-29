9b29391096324db81f36dd2a96157042dbdd3819
gAlvim <ledyzito@hotmail.com>
2024-10-29 18:50:22 -0300

    arruma o save_report

--- report.py
+++ report.py
@@ -2,6 +2,7 @@ from request_result import RequestResult, RequestResultList
 import os
 from typing import List
 from temporary_log import TemporaryLog
+from aps import Aps
 class Report(object):
 
     def __init__(self) -> None:
@@ -21,9 +22,11 @@ class Report(object):
 
     def save(self):
         temporary_log = TemporaryLog()
+        aps = Aps()
         for obj in self.__report:
             if obj.response_time !=0:
                 temporary_log.get_numbers_report(obj.response_time)
-        temporary_log.average_report()
+        temporary_log.avg_report()
+        aps.start(temporary_log.save_csv_log,2)
         temporary_log.save_csv_log()
               
