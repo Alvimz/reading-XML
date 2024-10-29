e7561c8941ed7a0e53589a4e122f413d5b15b6c9
gAlvim <ledyzito@hotmail.com>
2024-10-29 18:48:11 -0300

    add comentários e arruma temporary log

--- temporary_log.py
+++ temporary_log.py
@@ -3,7 +3,6 @@ from xlsx_manager import XlsxManager
 class TemporaryLog:
     def __init__(self) -> None:
         self.create_file_temporary()
-        self.number_of_request_to_save = 3
         self.report_numbers = list()
         
     def create_file_temporary(self):
@@ -24,37 +23,34 @@ class TemporaryLog:
     def save_csv_log(self):
         if self.len_txt_log():
             xls_manager = XlsxManager()
-            avg_numbers = self.avg_text()
+            avg_numbers = self.avg_txt()
         
             xls_manager.save_to_log(avg_numbers)
             self.clear_temporary_log()
         
             
-
+    #pega números do report!
     def get_numbers_report(self,number):
         self.report_numbers.append(number)
         return self.report_numbers   
-                
-    def average_report(self):
+    
+    
+    #faz a média do report!            
+    def avg_report(self):
         sum_numbers = sum(self.report_numbers)
         avg = sum_numbers/len(self.report_numbers)
         self.write_log(avg) # guarda a média no txt!
+        self.report_numbers.clear()        
     
-    
-    def len_txt_log(self)->bool:
-        count = 0
-        with open('log_temporary.txt', 'r') as f:
-            for _ in f:  
-                count += 1
-            if count > self.number_of_request_to_save:
-                return True  
-    
-    def avg_text(self):
+  
+    #faz a média do txt
+    def avg_txt(self):
         list_numbers = list()
         with open('log_temporary.txt', 'r') as f:
             for num in f:
                 list_numbers.append(float(num))
             avg = sum(list_numbers)/len(list_numbers)
+            list_numbers.clear()
             return avg
                 
    
