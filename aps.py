3264393df9403df8464b7489abcad83e0e8af005
gAlvim <ledyzito@hotmail.com>
2024-10-29 17:18:25 -0300

    cria o scheduler

---
+++ aps.py
@@ -0,0 +1,15 @@
+from apscheduler.schedulers.background import BackgroundScheduler
+
+class Aps:
+    
+    def __init__(self) -> None:
+        self.scheduler = BackgroundScheduler()
+    
+    def start(self,func,minutes=1):
+        self.scheduler.add_job(func,'interval',minutes)
+        self.scheduler.start()
+        
+    def stop(self):
+        self.scheduler.shutdown()
+        print('Parando o trabalho')
+    
